13ca61590f1ce93765ff3f639f923665d5fe2adf
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-04 11:16:26 +0200

    made public

---
+++ R/border_communes.R
@@ -0,0 +1,11 @@
+#' Norwegian cities
+#'
+#' Norwegian cities
+#'
+#' @docType data
+#' @name border_communes
+#' @format A rda file
+#' @keywords datasets
+#' @references to be added
+#' @source to be added
+NULL
